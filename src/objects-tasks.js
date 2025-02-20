5dfffd8aa2234cd95fd15d750a4b0ac270835534
Regina Moiseeva <moiseevaryu@gmail.com>
2025-02-20 14:43:11 +0500

    feat: add solution to Rectangle & getJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -163,8 +163,8 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return { width, height, getArea: () => width * height };
 }
 
 /**
@@ -177,8 +177,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
